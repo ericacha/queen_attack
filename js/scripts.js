e2c612e9b13aa0e7f2d33e95062e2c9e45acd589
Liz Beacham & Erica Cha <student@epicodus.com>
2015-04-06 15:36:11 -0700

    Finish writing logic into queenAttack method to account for diagonal moves, with passing test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var queenAttack = function queenAttack(queenMove, opponentMove) {
     if ((queenMove[0] === opponentMove[0] || queenMove[1] === opponentMove[1])) {
         return true;
+    } else if(Math.abs((queenMove[0] - opponentMove[0])) === Math.abs((queenMove[1] - opponentMove[1]))) {
+        return true;
     } else {
         return false;
     }
--- spec/specs.js
+++ spec/specs.js
@@ -3,7 +3,15 @@ describe('queenAttack', function() {
     expect(queenAttack([1, 1], [2, 3])).to.equal(false);
   });
 
-  it('is true if the coordinates are horizontally, vertically, or diagonally in line with each other', function() {
+  it('is true if the coordinates are horizontally in line with each other', function() {
     expect(queenAttack([1, 1], [1, 3])).to.equal(true);
+  });
+
+  it('is true if the coordinates are vertically in line with each other', function() {
+    expect(queenAttack([2, 1], [1, 1])).to.equal(true);
+  });
+
+  it('is true if the coordinates are diagonally in the line with each other', function() {
+      expect(queenAttack([7,6], [6,5])).to.equal(true);
   });
 });
